2120c35fee31deb32788681dcb260345f8dacf28
Rusu Darius <dariusrusu7@gmail.com>
2024-04-30 15:12:05 +0300

    Player & Position classes

---
+++ ChessLogic/Position.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public Player SquareColor()
+        {
+            if ((Row + Column) % 2 == 0)
+            {
+                return Player.White;
+            }
+            return Player.Black;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Position position && Row == position.Row && Column == position.Column;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Row, Column);
+        }
+
+        // For ease to compare positions
+        public static bool operator ==(Position left, Position right)
+        {
+            return EqualityComparer<Position>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Position left, Position right)
+        {
+            return !(left == right);
+        }
+    }
+}
