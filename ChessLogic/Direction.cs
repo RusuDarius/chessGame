f30fc71c4917e109eb1a0acdcc3f5162396224d1
Rusu Darius <dariusrusu7@gmail.com>
2024-04-30 20:22:38 +0300

    Direction class & position changes

---
+++ ChessLogic/Direction.cs
@@ -0,0 +1,38 @@
+namespace ChessLogic
+{
+    public class Direction
+    {
+        public static readonly Direction North = new Direction(-1, 0);
+        public static readonly Direction South = new Direction(1, 0);
+        public static readonly Direction East = new Direction(0, 1);
+        public static readonly Direction West = new Direction(0, -1);
+        public static readonly Direction NorthEast = North + East;
+        public static readonly Direction NorthWest = North + West;
+        public static readonly Direction SouthEast = South + East;
+        public static readonly Direction SouthWest = South + West;
+
+        public int RowDelta { get; }
+        public int ColumnDelta { get; }
+
+        public Direction(int rowDelta, int columnDelta)
+        {
+            RowDelta = rowDelta;
+            ColumnDelta = columnDelta;
+        }
+
+        //Posibility to add 2 directions together
+        public static Direction operator +(Direction dir1, Direction dir2)
+        {
+            return new Direction(
+                dir1.RowDelta + dir2.RowDelta,
+                dir1.ColumnDelta + dir2.ColumnDelta
+            );
+        }
+
+        // To scale a direction
+        public static Direction operator *(int scalar, Direction dir)
+        {
+            return new Direction(scalar * dir.RowDelta, scalar * dir.ColumnDelta);
+        }
+    }
+}
--- ChessLogic/Position.cs
+++ ChessLogic/Position.cs
@@ -45,5 +45,10 @@ namespace ChessLogic
         {
             return !(left == right);
         }
+
+        public static Position operator +(Position pos, Direction dir)
+        {
+            return new Position(pos.Row + dir.RowDelta, pos.Column + dir.ColumnDelta);
+        }
     }
 }
