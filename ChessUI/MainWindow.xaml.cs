c84f3af2b7b29699fcebaf6be95a676dbf564eaf
RusuDarius <dariusrusu7@gmail.com>
2024-05-01 21:23:21 +0300

    Added WPF UI project and drew the board & pieces

---
+++ ChessUI/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using ChessLogic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ChessUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Image[,] pieceImages = new Image[8, 8];
+        private GameState gameState;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeBoard();
+
+            gameState = new GameState(Player.White, Board.InitialBoardState());
+            DrawBoard(gameState.Board);
+        }
+  
+
+        private void InitializeBoard()
+        {
+            for(int r = 0; r < 8; r++)
+            {
+                for(int c = 0; c < 8; c++)
+                {
+                    Image image = new Image();
+                    pieceImages[r, c] = image;
+                    PieceGrid.Children.Add(image);
+                }
+            }
+        }
+
+        private void DrawBoard(Board board)
+        {
+            for(int r = 0; r < 8; r++)
+            {
+                for(int c = 0; c < 8; c++)
+                {
+                    Piece piece = board[r, c];
+                    pieceImages[r,c].Source = Images.GetImage(piece);
+                }
+            }
+        }
+    }
+}
