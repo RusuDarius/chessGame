b8ec03c340932a23f53eeeae1c8ec8420306fa07
RusuDarius <dariusrusu7@gmail.com>
2024-05-03 15:29:14 +0300

    Implemented checkmate & stalemate

---
+++ ChessLogic/EndReason.cs
@@ -0,0 +1,8 @@
+﻿namespace ChessLogic
+{
+    public enum EndReason
+    {
+        Checkmate,
+        Stalemate
+    }
+}
--- ChessLogic/GameState.cs
+++ ChessLogic/GameState.cs
@@ -4,6 +4,7 @@ namespace ChessLogic
     {
         public Board? Board { get; }
         public Player CurrentPlayer { get; private set; }
+        public Result Result { get; private set; } = null;
 
         // player + board as params because white starts first
         public GameState(Player player, Board board)
@@ -31,6 +32,39 @@ namespace ChessLogic
         {
             move.Execute(Board);
             CurrentPlayer = CurrentPlayer.Opponent();
+            CheckForGameOver();
+        }
+
+        // For verifying checkmate / stalemate, first generate all moves possible and filter the legal ones
+        public IEnumerable<Move> AllLegalMovesFor(Player player)
+        {
+            IEnumerable<Move> moveCandidates = Board.PiecePositionsFor(player).SelectMany(pos =>
+            {
+                Piece piece = Board[pos];
+                return piece.GetMoves(pos, Board);
+            });
+
+            return moveCandidates.Where(move => move.IsLegal(Board));
+        }
+
+        private void CheckForGameOver()
+        {
+            if (!AllLegalMovesFor(CurrentPlayer).Any())
+            {
+                if(Board.IsInCheck(CurrentPlayer))
+                {
+                    Result = Result.Win(CurrentPlayer.Opponent());
+                }
+                else
+                {
+                    Result = Result.Draw(EndReason.Stalemate);
+                }
+            }
+        }
+
+        public bool IsGameOver()
+        {
+            return Result != null;
         }
     }
 }
