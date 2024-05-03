c434488a0a9336b3a9cbf9b80ea248ea7c165ad9
RusuDarius <dariusrusu7@gmail.com>
2024-05-03 17:51:03 +0300

    Implemented custom winner & draw text

--- ChessUI/GameOverMenu.xaml.cs
+++ ChessUI/GameOverMenu.xaml.cs
@@ -1,19 +1,6 @@
 ﻿using ChessLogic;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ChessUI
 {
@@ -24,9 +11,13 @@ namespace ChessUI
     {
         public event Action<Option> OptionSelected;
 
-        public GameOverMenu()
+        public GameOverMenu(GameState gameState)
         {
             InitializeComponent();
+
+            Result result = gameState.Result;
+            WinnerText.Text = GetWinnerText(result.Winner);
+            ReasonText.Text = GetReasonText(result.Reason, gameState.CurrentPlayer);
         }
 
         private static string GetWinnerText(Player winner)
